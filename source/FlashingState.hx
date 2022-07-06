977d0225989b4df396d3090049b5be98d4828129
dokilon <taironporquesi@gmail.com>
2022-07-06 14:17:36 -0500

    aj

--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -24,11 +24,11 @@ class FlashingState extends MusicBeatState
 		add(bg);
 
 		warnText = new FlxText(0, 0, FlxG.width,
-			"Hey, watch out!\n
-			This Mod contains some flashing lights!\n
-			Press ENTER to disable them now or go to Options Menu.\n
-			Press ESCAPE to ignore this message.\n
-			You've been warned!",
+			"hola soy doki y esto es\n
+			fnf vs lolo guy remastered\n
+			este mod es bien xd esta medio\n
+			en espanol y medio en ingles weno es todo.\n
+			pulsa enter para continuar!",
 			32);
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
